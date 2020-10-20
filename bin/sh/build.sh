1b0ef08aa446424dc31f07eb118e8a345b61c72f
Bunlong <bunlong.van@gmail.com>
2020-10-20 21:48:10 +0700

    :sparkles:

---
+++ bin/sh/build.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+rm -rf dist/* \
+&& echo "Compiling..." \
+&& ./node_modules/.bin/google-closure-compiler \
+--compilation_level=SIMPLE \
+--js=node_modules/google-closure-library/closure/goog/array/array.js \
+--js=node_modules/google-closure-library/closure/goog/asserts/asserts.js \
+--js=node_modules/google-closure-library/closure/goog/base.js \
+--js=node_modules/google-closure-library/closure/goog/debug/error.js \
+--js=node_modules/google-closure-library/closure/goog/dom/asserts.js \
+--js=node_modules/google-closure-library/closure/goog/dom/htmlelement.js \
+--js=node_modules/google-closure-library/closure/goog/dom/nodetype.js \
+--js=node_modules/google-closure-library/closure/goog/dom/safe.js \
+--js=node_modules/google-closure-library/closure/goog/dom/tagname.js \
+--js=node_modules/google-closure-library/closure/goog/dom/tags.js \
+--js=node_modules/google-closure-library/closure/goog/fs/blob.js \
+--js=node_modules/google-closure-library/closure/goog/fs/url.js \
+--js=node_modules/google-closure-library/closure/goog/functions/functions.js \
+--js=node_modules/google-closure-library/closure/goog/html/safehtml.js \
+--js=node_modules/google-closure-library/closure/goog/html/safescript.js \
+--js=node_modules/google-closure-library/closure/goog/html/safestyle.js \
+--js=node_modules/google-closure-library/closure/goog/html/safestylesheet.js \
+--js=node_modules/google-closure-library/closure/goog/html/safeurl.js \
+--js=node_modules/google-closure-library/closure/goog/html/trustedresourceurl.js \
+--js=node_modules/google-closure-library/closure/goog/html/trustedtypes.js \
+--js=node_modules/google-closure-library/closure/goog/html/uncheckedconversions.js \
+--js=node_modules/google-closure-library/closure/goog/i18n/bidi.js \
+--js=node_modules/google-closure-library/closure/goog/labs/useragent/browser.js \
+--js=node_modules/google-closure-library/closure/goog/labs/useragent/util.js \
+--js=node_modules/google-closure-library/closure/goog/object/object.js \
+--js=node_modules/google-closure-library/closure/goog/proto2/descriptor.js \
+--js=node_modules/google-closure-library/closure/goog/proto2/fielddescriptor.js \
+--js=node_modules/google-closure-library/closure/goog/proto2/lazydeserializer.js \
+--js=node_modules/google-closure-library/closure/goog/proto2/message.js \
+--js=node_modules/google-closure-library/closure/goog/proto2/pbliteserializer.js \
+--js=node_modules/google-closure-library/closure/goog/proto2/serializer.js \
+--js=node_modules/google-closure-library/closure/goog/string/const.js \
+--js=node_modules/google-closure-library/closure/goog/string/internal.js \
+--js=node_modules/google-closure-library/closure/goog/string/string.js \
+--js=node_modules/google-closure-library/closure/goog/string/stringbuffer.js \
+--js=node_modules/google-closure-library/closure/goog/string/typedstring.js \
+--js=src/i18n/phonenumbers/index.js \
+--js=src/i18n/phonenumbers/metadata.js \
+--js=src/i18n/phonenumbers/phonemetadata.pb.js \
+--js=src/i18n/phonenumbers/phonenumber.pb.js \
+--js=src/i18n/phonenumbers/phonenumberutil.js \
+--js=src/i18n/phonenumbers/shortnumberinfo.js \
+--js=src/i18n/phonenumbers/shortnumbermetadata.js \
+--js=src/i18n/phonenumbers/asyoutypeformatter.js \
+--js_output_file=dist/libphonenumbers.original.js \
+&& ./node_modules/.bin/browserify dist/libphonenumbers.original.js --standalone libphonenumbers --no-browser-field --outfile dist/libphonenumbers.js \
+&& rm dist/libphonenumbers.original.js \
+&& echo "Build completed!"
---
+++ src/i18n/phonenumbers/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Require dependencies.
+
+goog.require('i18n.phonenumbers.PhoneNumberUtil');
+goog.require('i18n.phonenumbers.ShortNumberInfo');
+goog.require('i18n.phonenumbers.AsYouTypeFormatter');
+
+// Export libphonenumbers
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+exports.default = i18n.phonenumbers;
+
+module.exports = exports['default'];
